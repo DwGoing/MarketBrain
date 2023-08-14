612b70597b2029fb6b93b53c9e692d7785955f83
dwgoing <jianghy1209@gmail.com>
2023-08-15 00:31:22 +0800

    🧪 test: 提交测试

--- pkg/hd_wallet/hd_wallet.go
+++ pkg/hd_wallet/hd_wallet.go
@@ -3,6 +3,7 @@ package hd_wallet
 import (
 	"crypto/ecdsa"
 	"errors"
+	"fmt"
 
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcutil/hdkeychain"
@@ -14,17 +15,23 @@ type HDWallet struct {
 	masterKey *hdkeychain.ExtendedKey
 }
 
+type Currency int64
+
+const (
+	Currency_ETH Currency = 60
+)
+
 /*
 @title 	从种子导入钱包
-@param 	seed   	[]byte 		种子
-@return _ 		*HDWallet 	钱包实例
-@return _ 		error 		异常信息
+@param 	seed   			[]byte 			种子
+@return _ 				*HDWallet 		钱包实例
+@return _ 				error 			异常信息
 */
 func FromSeed(seed []byte) (*HDWallet, error) {
 	if len(seed) <= 0 {
 		return nil, errors.New("FromSeed Error: seed empty")
 	}
-	masterKey, err := hdkeychain.NewMaster(seed, &chaincfg.MainNetParams)
+	masterKey, err := hdkeychain.NewMaster(seed, &chaincfg.Params{HDPrivateKeyID: [4]byte{0x04, 0x88, 0xad, 0xe4}})
 	if err != nil {
 		return nil, err
 	}
@@ -35,10 +42,10 @@ func FromSeed(seed []byte) (*HDWallet, error) {
 
 /*
 @title 	从助记词导入钱包
-@param 	mnemonic   	string 		助记词
-@param 	password 	string 		密码
-@return _ 			*HDWallet 	钱包实例
-@return _ 			error 		异常信息
+@param 	mnemonic   		string 			助记词
+@param 	password 		string 			密码
+@return _ 				*HDWallet 		钱包实例
+@return _ 				error 			异常信息
 */
 func FromMnemonic(mnemonic string, password string) (*HDWallet, error) {
 	if mnemonic == "" {
@@ -56,15 +63,18 @@ func FromMnemonic(mnemonic string, password string) (*HDWallet, error) {
 
 /*
 @title 	派生子私钥
-@param 	Self   	*HDWallet 				HDWallet实例
-@param 	path 	accounts.DerivationPath 派生路径
-@return _		*ecdsa.PrivateKey 		子私钥
-@return _ 		error 					异常信息
+@param 	Self   	*HDWallet 			HDWallet实例
+@param 	path 	string				派生路径
+@return _		*ecdsa.PrivateKey 	子私钥
+@return _ 		error 				异常信息
 */
-func (Self *HDWallet) derivePrivateKey(path accounts.DerivationPath) (*ecdsa.PrivateKey, error) {
+func (Self *HDWallet) DerivePrivateKey(path string) (*ecdsa.PrivateKey, error) {
+	parsedPath, err := accounts.ParseDerivationPath(path)
+	if err != nil {
+		return nil, err
+	}
 	privateKey := Self.masterKey
-	var err error
-	for _, n := range path {
+	for _, n := range parsedPath {
 		privateKey, err = privateKey.Child(n)
 		if err != nil {
 			return nil, err
@@ -84,8 +94,8 @@ func (Self *HDWallet) derivePrivateKey(path accounts.DerivationPath) (*ecdsa.Pri
 @return _		*Account	Account实例
 @return _ 		error 		异常信息
 */
-func (Self *HDWallet) GetWallet(index uint32) (*Account, error) {
-	privateKey, err := Self.derivePrivateKey(append(accounts.DefaultRootDerivationPath, index))
+func (Self *HDWallet) GetAccount(currency Currency, index uint32) (*Account, error) {
+	privateKey, err := Self.DerivePrivateKey(fmt.Sprintf("m/44'/%d'/0'/0/%d", currency, index))
 	if err != nil {
 		return nil, err
 	}
