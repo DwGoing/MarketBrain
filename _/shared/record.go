7d01d25222b992b8178171d244b34d8b1b002664
dwgoing <jianghy1209@gmail.com>
2023-08-14 22:00:42 +0800

    🛠 miscellaneous: 同步blog进度

--- _/shared/record.go
+++
@@ -1,9 +0,0 @@
-package shared
-
-import "time"
-
-type Record struct {
-	Id        string    `gorm:"column:ID"`
-	CreatedAt time.Time `gorm:"column:CREATED_AT"`
-	UpdatedAt time.Time `gorm:"column:UPDATED_AT"`
-}
--- _/shared/request.go
+++
@@ -1,8 +0,0 @@
-package shared
-
-type Request struct{}
-
-type Response struct {
-	Code    int64  `json:"code,omitempty"`
-	Message string `json:"message,omitempty"`
-}
