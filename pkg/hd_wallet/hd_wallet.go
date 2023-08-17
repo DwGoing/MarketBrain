a3b799ffa61a34bc2bb34a98878ab31f88420973
dwgoing <jianghy1209@gmail.com>
2023-08-18 01:13:39 +0800

    ✨ feat: HD钱包支持BTC和ETH地址生成

--- pkg/hd_wallet/hd_wallet.go
+++ pkg/hd_wallet/hd_wallet.go
@@ -11,13 +11,31 @@ import (
 )
 
 type HDWallet struct {
-	masterKey *hdkeychain.ExtendedKey
+	seed []byte
 }
 
-type Currency int64
+type Version [2][4]byte
+
+var (
+	Version_xprv Version = [2][4]byte{{0x04, 0x88, 0xad, 0xe4}, {0x04, 0x88, 0xb2, 0x1e}}
+	Version_yprv Version = [2][4]byte{{0x04, 0x9d, 0x78, 0x78}, {0x04, 0x88, 0x7c, 0xb2}}
+	Version_Yprv Version = [2][4]byte{{0x02, 0x95, 0xb0, 0x05}, {0x04, 0x88, 0xb4, 0x3f}}
+	Version_zprv Version = [2][4]byte{{0x04, 0xb2, 0x43, 0x0c}, {0x04, 0x88, 0x47, 0x46}}
+	Version_Zprv Version = [2][4]byte{{0x02, 0xaa, 0x7a, 0x99}, {0x04, 0x88, 0x7e, 0xd3}}
+	Version_tprv Version = [2][4]byte{{0x04, 0x35, 0x83, 0x94}, {0x04, 0x88, 0x87, 0xcf}}
+	Version_uprv Version = [2][4]byte{{0x04, 0x4a, 0x4e, 0x28}, {0x04, 0x88, 0x52, 0x62}}
+	Version_Uprv Version = [2][4]byte{{0x02, 0x42, 0x85, 0xb5}, {0x04, 0x88, 0x89, 0xef}}
+	Version_vprv Version = [2][4]byte{{0x04, 0x5f, 0x18, 0xbc}, {0x04, 0x88, 0x1c, 0xf6}}
+	Version_Vprv Version = [2][4]byte{{0x02, 0x57, 0x50, 0x48}, {0x04, 0x88, 0x54, 0x83}}
+)
+
+type Currency int8
 
 const (
-	Currency_ETH Currency = 60
+	Currency_BTC_Legacy       Currency = 1
+	Currency_BTC_SegWit       Currency = 2
+	Currency_BTC_NativeSegWit Currency = 3
+	Currency_ETH              Currency = 4
 )
 
 /*
@@ -27,15 +45,11 @@ const (
 @return _ 				error 			异常信息
 */
 func FromSeed(seed []byte) (*HDWallet, error) {
-	if len(seed) <= 0 {
-		return nil, errors.New("FromSeed Error: seed empty")
-	}
-	masterKey, err := hdkeychain.NewMaster(seed, &chaincfg.Params{HDPrivateKeyID: [4]byte{0x04, 0x88, 0xad, 0xe4}})
-	if err != nil {
-		return nil, err
+	if len(seed) < 16 || len(seed) > 64 {
+		return nil, errors.New("seed invaild")
 	}
 	return &HDWallet{
-		masterKey: masterKey,
+		seed: seed,
 	}, nil
 }
 
@@ -62,45 +76,75 @@ func FromMnemonic(mnemonic string, password string) (*HDWallet, error) {
 
 /*
 @title 	派生子私钥
-@param 	Self   	*HDWallet 				HDWallet实例
-@param 	path 	string					派生路径
-@return _		*hdkeychain.ExtendedKey 子私钥
-@return _ 		error 					异常信息
+@param 	Self   		*HDWallet 				HDWallet实例
+@param 	version 	[4]byte					私钥ID
+@param 	path 		string					派生路径
+@return _			*hdkeychain.ExtendedKey 子私钥
+@return _ 			error 					异常信息
 */
-func (Self *HDWallet) DerivePrivateKey(path string) (*hdkeychain.ExtendedKey, error) {
+func (Self *HDWallet) DerivePrivateKey(version [4]byte, path string) (*hdkeychain.ExtendedKey, error) {
+	masterKey, err := hdkeychain.NewMaster(
+		Self.seed,
+		&chaincfg.Params{HDPrivateKeyID: version},
+	)
+	if err != nil {
+		return nil, err
+	}
 	parsedPath, err := accounts.ParseDerivationPath(path)
 	if err != nil {
 		return nil, err
 	}
-	privateKey := Self.masterKey
+	accountKey := masterKey
 	for _, n := range parsedPath {
-		privateKey, err = privateKey.Child(n)
+		accountKey, err = accountKey.Child(n)
 		if err != nil {
 			return nil, err
 		}
 	}
-	return privateKey, nil
+	return accountKey, nil
 }
 
 /*
 @title 	获取子钱包
-@param 	Self   	*HDWallet 	HDWallet实例
-@param 	index 	uint32 		钱包索引
-@return _		*Account	Account实例
-@return _ 		error 		异常信息
+@param 	Self   		*HDWallet 	HDWallet实例
+@param 	currency 	Currency 	版本
+@param 	coin 		int64 		币种
+@param 	index 		Currency 	钱包索引
+@return _			*Account	Account实例
+@return _ 			error 		异常信息
 */
-func (Self *HDWallet) GetAccount(currency Currency, index uint32) (*Account, error) {
-	privateKey, err := Self.DerivePrivateKey(fmt.Sprintf("m/44'/%d'/0'/0/%d", currency, index))
+func (Self *HDWallet) GetAccount(currency Currency, index int64) (*Account, error) {
+	var (
+		version Version
+		path    string
+	)
+	switch currency {
+	case Currency_BTC_Legacy:
+		version = Version_xprv
+		path = "m/44'/0'/0'/0/"
+	case Currency_BTC_SegWit:
+		version = Version_xprv
+		path = "m/49'/0'/0'/0/"
+	case Currency_BTC_NativeSegWit:
+		version = Version_xprv
+		path = "m/84'/0'/0'/0/"
+	case Currency_ETH:
+		version = Version_xprv
+		path = "m/44'/60'/0'/0/"
+	default:
+		return nil, errors.New("unsupportted currency")
+	}
+	path = fmt.Sprintf("%s%d", path, index)
+	privateKey, err := Self.DerivePrivateKey(version[0], path)
 	if err != nil {
 		return nil, err
 	}
-
 	btcecPrivateKey, err := privateKey.ECPrivKey()
 	if err != nil {
 		return nil, err
 	}
 	return &Account{
-		Index:      index,
-		PrivateKey: btcecPrivateKey.ToECDSA(),
+		Currency:   currency,
+		PrivateKey: btcecPrivateKey,
 	}, nil
 }
