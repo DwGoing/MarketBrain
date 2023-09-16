2d96692463b4a4fb493e86ed74ad035251c1979b
dwgoing <jianghy1209@gmail.com>
2023-09-17 01:23:51 +0800

    ✨ feat: 增加充值成功回调

---
+++ internal/funds_service/module/notify.go
@@ -0,0 +1,32 @@
+package module
+
+import (
+	"bytes"
+	"errors"
+	"net/http"
+)
+
+// +ioc:autowire=true
+// +ioc:autowire:type=normal
+type Notify struct {
+	Config *Config `normal:""`
+}
+
+// @title	发送通知
+// @param	Self	*Notify	模块实例
+// @param	url		string	回调Url
+// @param	data	[]byte	回调数据
+func (Self *Notify) Send(url string, data []byte) error {
+	request, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return err
+	}
+	if response.StatusCode != http.StatusOK {
+		return errors.New("request failed")
+	}
+	return nil
+}
