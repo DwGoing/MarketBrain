5b30bcfaf411a57a718aace227aa08ea11160caa
dwgoing <jianghy1209@gmail.com>
2023-09-20 23:44:31 +0800

    ✨ feat: 新增转账记录

--- internal/funds_service/module/chain.go
+++ internal/funds_service/module/chain.go
@@ -502,52 +502,86 @@ func (Self *Chain) GetBalance(chainType enum.ChainType, contract *string, wallet
 // @title	发送代币
 // @param	Self		*Chain				模块实例
 // @param	chainType	enum.ChainType		链类型
-// @param	contract	*string				合约地址
+// @param	token		*string				合约地址
 // @param	from		*hd_wallet.Account	发送账户
 // @param	to			string				接收地址
+// @param	amount		float64				数量
+// @param	remarks		string				备注
 // @return	_			error				异常信息
-func (Self *Chain) Transfer(chainType enum.ChainType, contract *string, from *hd_wallet.Account, to string, amount float64) (string, error) {
-	configModule, _ := GetConfig()
-	config, err := configModule.Load()
+func (Self *Chain) Transfer(chainType enum.ChainType, token *string, from *hd_wallet.Account, to string, amount float64, remarks string) (string, error) {
+	storageModule, _ := GetStorage()
+	mysqlClient, err := storageModule.GetMysqlClient()
 	if err != nil {
 		return "", err
 	}
-	chainConfig, ok := config.ChainConfigs[chainType.String()]
-	if !ok || len(chainConfig.Nodes) < 1 {
-		return "", errors.New("no chain config")
+	db, err := mysqlClient.DB()
+	if err != nil {
+		return "", err
 	}
-	var txHash string
-	switch chainType {
-	case enum.ChainType_TRON:
-		client, err := Self.getTronClient(chainConfig)
+	defer db.Close()
+	txHash, err := func(chainType enum.ChainType, token *string, from *hd_wallet.Account, to string, amount float64) (string, error) {
+		configModule, _ := GetConfig()
+		config, err := configModule.Load()
 		if err != nil {
 			return "", err
 		}
-		var tx *api.TransactionExtention
-		if contract == nil {
-			amountInt64, _ := new(big.Float).Mul(big.NewFloat(amount), big.NewFloat(1e6)).Int64()
-			tx, err = client.Transfer(from.GetAddress(), to, amountInt64)
-			if err != nil {
-				return "", err
-			}
-		} else {
-			decimalsBigInt, err := client.TRC20GetDecimals(*contract)
-			if err != nil {
-				return "", err
-			}
-			amountBigInt, _ := new(big.Float).Mul(big.NewFloat(amount), big.NewFloat(math.Pow10(int(decimalsBigInt.Int64())))).Int(new(big.Int))
-			tx, err = client.TRC20Send(from.GetAddress(), to, *contract, amountBigInt, 300000000)
-			if err != nil {
-				return "", err
-			}
+		chainConfig, ok := config.ChainConfigs[chainType.String()]
+		if !ok || len(chainConfig.Nodes) < 1 {
+			return "", errors.New("no chain config")
 		}
-		txInfo, err := Self.sendTronTransaction(client, from.PrivateKey.ToECDSA(), tx.Transaction, true)
-		if err != nil {
-			return "", err
+		var txHash string
+		switch chainType {
+		case enum.ChainType_TRON:
+			client, err := Self.getTronClient(chainConfig)
+			if err != nil {
+				return "", err
+			}
+			var tx *api.TransactionExtention
+			if token == nil {
+				amountInt64, _ := new(big.Float).Mul(big.NewFloat(amount), big.NewFloat(1e6)).Int64()
+				tx, err = client.Transfer(from.GetAddress(), to, amountInt64)
+				if err != nil {
+					return "", err
+				}
+			} else {
+				decimalsBigInt, err := client.TRC20GetDecimals(*token)
+				if err != nil {
+					return "", err
+				}
+				amountBigInt, _ := new(big.Float).Mul(big.NewFloat(amount), big.NewFloat(math.Pow10(int(decimalsBigInt.Int64())))).Int(new(big.Int))
+				tx, err = client.TRC20Send(from.GetAddress(), to, *token, amountBigInt, 300000000)
+				if err != nil {
+					return "", err
+				}
+			}
+			txInfo, err := Self.sendTronTransaction(client, from.PrivateKey.ToECDSA(), tx.Transaction, true)
+			if err != nil {
+				return "", err
+			}
+			txHash = common.Bytes2Hex(txInfo.GetId())
+		default:
+			return "", errors.New("unsupported chain type")
 		}
-		txHash = common.Bytes2Hex(txInfo.GetId())
-	default:
-		return "", errors.New("unsupported chain type")
+		return txHash, nil
+	}(chainType, token, from, to, amount)
+	// 创建日志
+	record := model.TransferRecord{
+		ChainType:   chainType.String(),
+		Token:       token,
+		FromIndex:   from.Index,
+		FromAddress: from.GetAddress(),
+		To:          to,
+		Amount:      amount,
+		Status:      enum.TransferStatus_SUCCESS.String(),
+		Remarks:     remarks,
 	}
-	return txHash, nil
+	if err != nil {
+		record.Status = enum.TransferStatus_FAILED.String()
+		record.Error = err.Error()
+	}
+	_, err = model.CreateTransferRecord(mysqlClient, &record)
+	if err != nil {
+		return "", err
+	}
+	return txHash, err
 }
