23e54b0032a96aa8395664402eba6d98ccd5a412
dwgoing <jianghy1209@gmail.com>
2023-09-29 02:11:20 +0800

    🔧 build: 更新数据库文件

--- MARKET_BRAIN.sql
+++ MARKET_BRAIN.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80033
  File Encoding         : 65001
  
- Date: 21/09/2023 00:26:41
+ Date: 29/09/2023 02:10:33
  */
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
@@ -31,12 +31,17 @@ BEGIN;
 INSERT INTO `CONFIG` (`KEY`, `VALUE`)
 VALUES (
     'CHAIN_CONFIGS',
-    '{\"TRON\":{\"usdt\":\"\",\"nodes\":[\"grpc.nile.trongrid.io:50051\"],\"apiKey\":\"\"}}'
+    '{\"TRON\":{\"usdt\":\"TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj\",\"nodes\":[\"grpc.nile.trongrid.io:50051\"],\"apiKey\":\"d9b77ec9-39e0-4765-98d8-2c59188344a0\"}}'
   );
+INSERT INTO `CONFIG` (`KEY`, `VALUE`)
+VALUES ('EXPIRE_TIME', '15');
 INSERT INTO `CONFIG` (`KEY`, `VALUE`)
 VALUES ('MIN_GAS_THRESHOLD', '20');
 INSERT INTO `CONFIG` (`KEY`, `VALUE`)
-VALUES ('MNEMONIC', '\"\"');
+VALUES (
+    'MNEMONIC',
+    '\"math absorb sweet shrimp time smoke net pulp carbon gorilla expand payment\"'
+  );
 INSERT INTO `CONFIG` (`KEY`, `VALUE`)
 VALUES ('TRANSFER_GAS_AMOUNT', '50');
 INSERT INTO `CONFIG` (`KEY`, `VALUE`)
@@ -54,7 +59,7 @@ CREATE TABLE `RECHARGE_ORDER` (
   `EXTERNAL_DATA` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
   `CALLBACK_URL` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `CHAIN_TYPE` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `AMOUNT` decimal(10, 0) NOT NULL,
+  `AMOUNT` decimal(64, 18) NOT NULL,
   `WALLET_INDEX` bigint NOT NULL,
   `WALLET_ADDRESS` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `STATUS` varchar(255) CHARACTER SET utf8mb3 NOT NULL,
@@ -82,7 +87,7 @@ CREATE TABLE `TRANSFER` (
   `FROM_INDEX` bigint NOT NULL,
   `FROM_ADDRESS` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   `TO` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  `AMOUNT` decimal(10, 0) NOT NULL,
+  `AMOUNT` decimal(64, 18) NOT NULL,
   `STATUS` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `ERROR` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci,
   `REMARKS` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
